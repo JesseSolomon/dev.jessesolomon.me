9f605873bedf436be511c649f3bfac3e01cbbc72
Jesse Solomon <jessedsolomon@gmail.com>
2022-09-23 16:05:35 +0000

    Start on new responsive section

--- dist/js/app.js
+++ dist/js/app.js
@@ -258,14 +258,42 @@ class NWAElement extends HTMLElement {
     }
 }
 class ResponsiveElement extends HTMLElement {
-    breakpoints() {
+    constructor() {
+        super(...arguments);
+        this.sizingMode = "auto";
+        this.currentSize = 1366;
+    }
+    updateSizing() {
         const { width } = this.page.getBoundingClientRect();
-        this.classList.toggle("break--header", width <= 630);
-        requestAnimationFrame(() => this.breakpoints());
+        let targetSize = 0;
+        switch (this.sizingMode) {
+            case "auto":
+                const { height, top } = this.getBoundingClientRect();
+                const progress = Math.max(-top, 0) / height;
+                targetSize = 1200 * (1 - progress) + 380 * progress;
+                break;
+            case "desktop":
+                targetSize = 1200;
+                break;
+            case "tablet":
+                targetSize = 800;
+                break;
+            case "phone":
+                targetSize = 380;
+                break;
+        }
+        this.currentSize += Math.min(Math.abs(this.currentSize - targetSize), 30) * Math.sign(targetSize - this.currentSize);
+        this.style.setProperty("--simulated-viewport-width", this.currentSize.toString() + "px");
+        requestAnimationFrame(() => this.updateSizing());
     }
     connectedCallback() {
         this.page = this.querySelector(".responsive--page");
-        this.breakpoints();
+        this.querySelectorAll("button").forEach(button => button.addEventListener("click", () => {
+            this.sizingMode = button.name;
+            button.parentElement.querySelector(".current").classList.remove("current");
+            button.classList.add("current");
+        }));
+        this.updateSizing();
     }
 }
 var ScrollBehavior;
--- ts/responsive.ts
+++ ts/responsive.ts
@@ -1,17 +1,46 @@
 class ResponsiveElement extends HTMLElement {
+	sizingMode: "auto" | "desktop" | "tablet" | "phone" = "auto";
+	currentSize: number = 1366;
 	page: HTMLElement;
 
-	breakpoints() {
+	updateSizing() {
 		const { width } = this.page.getBoundingClientRect();
 
-		this.classList.toggle("break--header", width <= 630);
+		let targetSize = 0;
 
-		requestAnimationFrame(() => this.breakpoints());
+		switch (this.sizingMode) {
+			case "auto":
+				const { height, top } = this.getBoundingClientRect();
+				const progress = Math.max(-top, 0) / height;
+				
+				targetSize = 1200 * (1 - progress) + 380 * progress;
+				break;
+			case "desktop":
+				targetSize = 1200;
+				break;
+			case "tablet":
+				targetSize = 800;
+				break;
+			case "phone":
+				targetSize = 380;
+				break;
+		}
+
+		this.currentSize += Math.min(Math.abs(this.currentSize - targetSize), 30) * Math.sign(targetSize - this.currentSize);
+		this.style.setProperty("--simulated-viewport-width", this.currentSize.toString() + "px");
+
+		requestAnimationFrame(() => this.updateSizing());
 	}
 
 	connectedCallback() {
 		this.page = this.querySelector(".responsive--page");
 
-		this.breakpoints();
+		this.querySelectorAll("button").forEach(button => button.addEventListener("click", () => {
+			this.sizingMode = button.name as "auto" | "desktop" | "tablet" | "phone";
+			button.parentElement.querySelector(".current").classList.remove("current");
+			button.classList.add("current");
+		}));
+
+		this.updateSizing();
 	}
 }
